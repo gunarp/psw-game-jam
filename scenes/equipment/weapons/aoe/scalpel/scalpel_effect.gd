3c85c55abd538dcc301343ce8e2d3f97e5bbedea
gunarp <petergunarso@outlook.com>
2025-01-27 23:01:19 -0800

    add additional comment for personal remidner

--- scenes/equipment/weapons/aoe/scalpel/scalpel_effect.gd
+++ scenes/equipment/weapons/aoe/scalpel/scalpel_effect.gd
@@ -4,8 +4,6 @@ var base_stats: WeaponBaseStats
 var player_ref: PlayerEntity
 
 
-
-
 func _on_cooldown_timer_timeout() -> void:
   # activate effect
   # begin duration timer
@@ -14,4 +12,11 @@ func _on_cooldown_timer_timeout() -> void:
 func _on_duration_timer_timeout() -> void:
   # disble effect
   # begin cooldown timer
+	pass # Replace with function body.
+
+
+func _on_body_entered(body:Node2D) -> void:
+  # if body is an enemy type then apply hit effect
+  # also add enemy onto collision exception list.
+  # begin cooldown timer to remove exception from list as well
 	pass # Replace with function body.
