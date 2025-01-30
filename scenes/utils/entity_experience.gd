88ebba99c5c2fd1423f808aff2fdd03127b9fdd7
Sunny Hu <hu.sunnya@gmail.com>
2025-01-30 03:23:20 -0800

    changed level up signal name, added initial debugging code

--- scenes/utils/entity_experience.gd
+++ scenes/utils/entity_experience.gd
@@ -6,16 +6,18 @@ var current_experience: int
 var total_experience: int
 var exp_per_level: int = 50 # can be changed in the future
 
-signal level_up_emit
+signal exp_level_up
 
 func on_exp_gather(exp_gain : int):
 	total_experience += exp_gain
 	current_experience += exp_gain
 	
+	# currently there is no changing the exp_per_level value
+	# so the math below reflects that
 	if (current_experience >= exp_per_level):
 		current_experience -= exp_per_level
 		level += 1
-		emit_signal("level_up_emit")
+		exp_level_up.emit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,6 +26,11 @@ func _ready() -> void:
 	current_experience = 0
 
 
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# CURRENTLY BEING USED TO DEBUG.
+func _process(_delta: float) -> void:
+	on_exp_gather(10)
+	print("Exp. Gain: %d" % level)
+	print("Current Exp.: %d" % current_experience)
+	print("Total Exp.: %d" % total_experience)
+	pass
