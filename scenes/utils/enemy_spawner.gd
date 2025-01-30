605ef69b18cca6bb5f02857017f0deb2d69e956a
gunarp <petergunarso@outlook.com>
2025-01-30 02:11:06 -0800

    make enemies spawn only in currently accessible play area

--- scenes/utils/enemy_spawner.gd
+++ scenes/utils/enemy_spawner.gd
@@ -8,6 +8,7 @@ enum SPAWN_SIDE {LEFT, RIGHT, TOP, BOTTOM}
 @export var camera: Camera2D
 
 @onready var player: PlayerEntity = get_tree().get_first_node_in_group("player")
+@onready var space_state = player.get_world_2d().direct_space_state
 
 const margin_offset: Vector2 = Vector2(5, 5)
 
@@ -22,22 +23,29 @@ func change_mob_spawned(new_enemy: PackedScene) -> void:
 func _on_spawn_timer_timeout() -> void:
 	var newEnemy = mob_to_spawn.instantiate() as EnemyEntity
 	newEnemy.initialize(player, mob_scale)
-	newEnemy.position = _get_random_spawn_position()
+	newEnemy.global_position = _get_random_spawn_position()
 	get_tree().root.add_child(newEnemy)
 
 
 func _get_random_spawn_position() -> Vector2:
-	var side_to_spawn_on = SPAWN_SIDE.values().pick_random()
 	var camera_origin = camera.get_canvas_transform().affine_inverse().get_origin() + margin_offset
 	var camera_extent = camera_origin + camera.get_viewport_rect().size - 2 * margin_offset
 
 	var spawn_pos = Vector2()
-	match side_to_spawn_on:
-		SPAWN_SIDE.LEFT: spawn_pos = Vector2(camera_origin.x, randf_range(camera_origin.y, camera_extent.y))
-		SPAWN_SIDE.RIGHT: spawn_pos = Vector2(camera_extent.x, randf_range(camera_origin.y, camera_extent.y))
-		SPAWN_SIDE.TOP: spawn_pos = Vector2(randf_range(camera_origin.x, camera_extent.x), camera_origin.y)
-		SPAWN_SIDE.BOTTOM: spawn_pos = Vector2(randf_range(camera_origin.x, camera_extent.x), camera_extent.y)
+	
+	while (true):
+		var side_to_spawn_on = SPAWN_SIDE.values().pick_random()
+		match side_to_spawn_on:
+			SPAWN_SIDE.LEFT: spawn_pos = Vector2(camera_origin.x, randf_range(camera_origin.y, camera_extent.y))
+			SPAWN_SIDE.RIGHT: spawn_pos = Vector2(camera_extent.x, randf_range(camera_origin.y, camera_extent.y))
+			SPAWN_SIDE.TOP: spawn_pos = Vector2(randf_range(camera_origin.x, camera_extent.x), camera_origin.y)
+			SPAWN_SIDE.BOTTOM: spawn_pos = Vector2(randf_range(camera_origin.x, camera_extent.x), camera_extent.y)
 
-	#print("spawning mob at: ", spawn_pos, " Player at: ", camera.get_screen_center_position())
-	#print("Camera between these points: ", camera_origin, " ", camera_extent)
+		# Now query for if this generated point would intersect a wall
+		var query = PhysicsRayQueryParameters2D.create(spawn_pos, player.global_position)
+		query.collision_mask = 0x0001
+		query.hit_from_inside = true
+		if (space_state.intersect_ray(query).size() == 0):
+			break
+
 	return spawn_pos
