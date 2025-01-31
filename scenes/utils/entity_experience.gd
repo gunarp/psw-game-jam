d107c84dd6969489c6597068ccf555e5773ec550
Sunny Hu <hu.sunnya@gmail.com>
2025-01-31 00:32:09 -0800

    changing whitespace from tabs to spaces

--- scenes/utils/entity_experience.gd
+++ scenes/utils/entity_experience.gd
@@ -9,28 +9,29 @@ var exp_per_level: int = 50 # can be changed in the future
 signal exp_level_up
 
 func on_exp_gather(exp_gain : int):
-	total_experience += exp_gain
-	current_experience += exp_gain
-	
-	# currently there is no changing the exp_per_level value
-	# so the math below reflects that
-	if (current_experience >= exp_per_level):
-		current_experience -= exp_per_level
-		level += 1
-		exp_level_up.emit()
+  print("incrementing exp gain")
+  total_experience += exp_gain
+  current_experience += exp_gain
+  
+  # currently there is no changing the exp_per_level value
+  # so the math below reflects that
+  if (current_experience >= exp_per_level):
+    current_experience -= exp_per_level
+    level += 1
+    exp_level_up.emit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	level = 0
-	total_experience = 0
-	current_experience = 0
+  level = 0
+  total_experience = 0
+  current_experience = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # CURRENTLY BEING USED TO DEBUG.
 func _process(_delta: float) -> void:
-	on_exp_gather(10)
-	print("Exp. Gain: %d" % level)
-	print("Current Exp.: %d" % current_experience)
-	print("Total Exp.: %d" % total_experience)
-	pass
+  #on_exp_gather(10)
+  #print("Exp. Gain: %d" % level)
+  #print("Current Exp.: %d" % current_experience)
+  #print("Total Exp.: %d" % total_experience)
+  pass
