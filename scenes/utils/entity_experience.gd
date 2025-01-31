abefda263fd98296332b421ca445af1c7c62de64
gunarp <petergunarso@outlook.com>
2025-01-31 02:58:58 -0800

    hook experience to UI experience bar

--- scenes/utils/entity_experience.gd
+++ scenes/utils/entity_experience.gd
@@ -1,37 +1,21 @@
 extends Node
 
 class_name EntityExperience
-var level: int
-var current_experience: int
-var total_experience: int
-var exp_per_level: int = 50 # can be changed in the future
+@onready var level: int = 0
+@onready var current_experience: int
+@onready var total_experience: int
 
-signal exp_level_up
+@onready var exp_per_level = [50, 100, 100, 300, 300, 500, 500]
+@onready var level_up_threshhold: int = 50
 
-func on_exp_gather(exp_gain : int):
-  print("incrementing exp gain")
+signal exp_level_up(level_up_threshhold: float)
+
+func on_exp_gather(exp_gain: int):
   total_experience += exp_gain
   current_experience += exp_gain
 
-  # currently there is no changing the exp_per_level value
-  # so the math below reflects that
-  if (current_experience >= exp_per_level):
-    current_experience -= exp_per_level
+  if (current_experience >= level_up_threshhold):
+    current_experience = 0
     level += 1
-    exp_level_up.emit()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  level = 0
-  total_experience = 0
-  current_experience = 0
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# CURRENTLY BEING USED TO DEBUG.
-func _process(_delta: float) -> void:
-  #on_exp_gather(10)
-  #print("Exp. Gain: %d" % level)
-  #print("Current Exp.: %d" % current_experience)
-  #print("Total Exp.: %d" % total_experience)
-  pass
+    level_up_threshhold = exp_per_level[level] if level < exp_per_level.size() else exp_per_level[-1]
+    exp_level_up.emit(level_up_threshhold)
