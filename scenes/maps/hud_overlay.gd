646119eee6c31b2309201092db3443a7bd3872d2
gunarp <petergunarso@outlook.com>
2025-01-31 05:49:27 -0800

    add signals

--- scenes/maps/hud_overlay.gd
+++ scenes/maps/hud_overlay.gd
@@ -10,9 +10,9 @@ extends CanvasLayer
 
 func _ready() -> void:
   hud_image.position = Vector2(320, 240)  # Adjust HUD (10, 10) for preferred position
-  #experience_bar.position = Vector2(120, 440) 
-  level_up_ui.option_selected.connect(_add_icon_to_hud)
-  
+  #experience_bar.position = Vector2(120, 440)
+  # level_up_ui.option_selected.connect(_add_icon_to_hud)
+
 func _add_icon_to_hud(icon_texture: Texture2D):
   var new_icon = TextureRect.new()
   new_icon.texture = icon_texture
