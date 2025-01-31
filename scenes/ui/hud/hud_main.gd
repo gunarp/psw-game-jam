9a834829a2a9d6207c8f9cb20a545b22e772823d
Addison <arcothepyro@gmail.com>
2025-01-30 21:26:20 -0800

    basic hud

--- scenes/ui/hud/hud_main.gd
+++ scenes/ui/hud/hud_main.gd
@@ -6,9 +6,10 @@ extends CanvasLayer
 #@onready var health_bar_offset: Vector2 = Vector2(0, 0)
 @onready var health_bar_scale: Vector2 = Vector2(0.4, 0.4)
 
+
 func _ready() -> void:
 	$HealthBar.set_position((get_viewport().get_visible_rect().size / 2) + (health_bar_offset * health_bar_scale))
 	$HealthBar.scale = health_bar_scale
 
-func _process(_delta: float) -> void:
+func _process(_delta):
 	pass
